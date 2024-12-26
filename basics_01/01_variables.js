a7634a454c4d33ec559fff8bbdeb88fdd0229b02
Mas-Kode-er <varun.pandey.bly@gmail.com>
2024-12-26 20:20:48 +0000

    learning varibles

---
+++ basics_01/01_variables.js
@@ -0,0 +1,22 @@
+var a = 1
+var a = 2 // can be redeclared and updated as well 
+
+if (1) {
+    var a = 5
+    console.log(a)
+
+}
+console.log(a);
+// gives 5 5 in the output ...
+
+const accountId = 2345 // this will remain constant throughout the program..
+// this cant be updated 
+let name; // initialised as undefined..
+
+// we can also declare variables in js without any keywords..
+
+accountcity = "Bareilly"
+
+// using table format of the console to print multiple var at a time 
+
+console.table([a,accountcity,accountId,name])
