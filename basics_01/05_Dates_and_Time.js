b5f21cf1e5d678d9a7107fcfdcaf932cdbfee18d
Mas-Kode-er <varun.pandey.bly@gmail.com>
2024-12-30 10:31:41 +0000

    added a new line of code

---
+++ basics_01/05_Dates_and_Time.js
@@ -0,0 +1,10 @@
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++  Dates +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const date = new Date() // date ek object create kia hai humne class Date{} ka 
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+
+console.log(typeof(date));
+console.log("commit");
+
